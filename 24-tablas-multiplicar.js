37dd58de0e74efe6b704dfbf9d995014889a09b9
vainimilk <yessyherrera.h@gmail.com>
2024-06-05 09:41:10 +0200

    Sustituimos el (console.log) y guardamos los resultados en memoria en un array

--- 24-tablas-multiplicar.js
+++ 24-tablas-multiplicar.js
@@ -1,6 +1,7 @@
+var resultados = []
+
 for (let tabla = 1; tabla <= 10; tabla++){
     for (let j =1 ; j <=10; j++){
-        console.log(tabla + "x" + j + "=" + (tabla*j))
+        resultados.push(tabla + "x" + j + "=" + (tabla*j))
     }
-
 }
